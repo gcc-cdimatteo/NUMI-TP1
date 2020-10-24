7e4b5a1974d273057db158488437d61e9b1063ec
ccccoti <m.constanza.weber@gmail.com>
2020-10-24 18:50:34 -0300

    agregado de ejercicios e f y g

--- ejercicios.m
+++ ejercicios.m
@@ -1,4 +1,3 @@
-
 a = "";
 
 function ejercicio_A();
@@ -21,24 +20,6 @@ endfunction
 
 
 function ejercicio_B();
-  
-##  datos1 = load('EnergiasRenovablesSimple.dat');
-##  datos2 = load('DemandaSimple.dat');
-##  
-##  i = 1;
-##  j = 1;
-##  n=rows(datos1)
-##  while i <= n
-##    suma(j,1) = datos1(i,6);
-##    i = i + 1 ;
-##    while i <= n && datos1(i,1) == datos1(i-1,1) && datos1(i,2) == datos1(i-1,2) 
-##      suma(j,1) = suma(j,1) + datos1(i,6);
-##      i = i + 1;
-##    endwhile
-##    j = j + 1;
-##  endwhile
-##  disp(suma)
-
   ofe = load('EnergiasRenovablesSimple.dat');
   dem = load('DemandaSimple.dat');
   n = rows(ofe);
@@ -86,10 +67,9 @@ function ejercicio_B();
 endfunction
 
 function ejercicio_C();
-  
-##  En este ejercicio habia un par de endif´s que no le pusieron condicion,
-##  y por eso tiraba error en el command window y pedia parentesis. 
-##  Solo habia que cambiarlos por else.
+#  En este ejercicio habia un par de endif´s que no le pusieron condicion,
+#  y por eso tiraba error en el command window y pedia parentesis. 
+#  Solo habia que cambiarlos por else.
   
   ofe = load('EnergiasRenovablesSimple.dat');
   dem = load('DemandaSimple.dat');
@@ -121,22 +101,22 @@ function ejercicio_C();
     ylabel("Consumo respecto al total  [%] ")
     legend("Eolica","Hidraulica","Eolica + Hidraulica",'location','north')
 
-##  Totales = plot(years,Data(:,2),"r*-",years,Data(:,4),"b*-",years,Data(:,6),"g*-")
-##  Porcentajes = plot(years,Data(:,3),"r*-",years,Data(:,5),"b*-",years,Data(:,7),"g*-")
+#  Totales = plot(years,Data(:,2),"r*-",years,Data(:,4),"b*-",years,Data(:,6),"g*-")
+#  Porcentajes = plot(years,Data(:,3),"r*-",years,Data(:,5),"b*-",years,Data(:,7),"g*-")
   
   
   
 endfunction
 
 function res = pow_an_tipo(cod,ofe) 
-##  Esta funcion devuelve una matriz de nx2 = [año_j oferta_anual] 
-##  para el tipo que se especifique segun el código:
-##    1    BIODIESEL
-##    2    BIOGAS
-##    3    BIOMASA
-##    4    EOLICO
-##    5    HIDRO <=50MW
-##    6    SOLAR  
+#  Esta funcion devuelve una matriz de nx2 = [año_j oferta_anual] 
+#  para el tipo que se especifique segun el código:
+#    1    BIODIESEL
+#    2    BIOGAS
+#    3    BIOMASA
+#    4    EOLICO
+#    5    HIDRO <=50MW
+#    6    SOLAR  
 n = rows(ofe);
 year  = ofe(1:n,1);
 pow   = ofe(1:n,6);
@@ -156,7 +136,7 @@ endfor
 endfunction
 
 function res = demanda_anual(dem)
-##  Esta funcion devuelve una matriz de 2xn = [año_j demanda_anual]
+#  Esta funcion devuelve una matriz de 2xn = [año_j demanda_anual]
   n     = rows(dem);
   year  = dem(:,1);
   res   = [year(1) 0];
@@ -170,6 +150,7 @@ for i = 1:n;
   res(j,2)+= dem(i,3);
 endfor
 endfunction
+
 function ejercicio_D();
   
   datos1 = load('EnergiasRenovablesSimple.dat');
@@ -284,41 +265,151 @@ function ejercicio_D();
   disp(porcentaje3)
 endfunction
 
-function ejercicio_E();
-  
+function pos = existeEnMatriz(matriz, valores, columnas);
+  pos = -1;
+  for i = 1:rows(matriz)
+    j = columnas(1, 1);
+    while j >= 0 && j <= columns(columnas) && pos == -1
+      if matriz(i, columnas(1, j)) == valores(1, columnas(1, j))
+        if j == columns(columnas) pos = i; endif
+        j += 1;
+      else
+        j = -1;
+      endif
+    endwhile
+  endfor
+endfunction
+
+function ejercicio_E()
+  energiasRenovables = load('EnergiasRenovablesSimple.dat');
+  aportesAnualesFuente = [];
+  aportesAnualesRegion = [];
+  contFuente = 1;
+  contRegion = 1;
+  for i = 1:rows(energiasRenovables)
+    anio = energiasRenovables(i,1);
+    fuente = energiasRenovables(i,4);
+    aporte = energiasRenovables(i,6);
+    existePos = existeEnMatriz(aportesAnualesFuente, [ anio fuente ], [ 1 2 ]);
+    if existePos != -1
+      aportesAnualesFuente(existePos,3) += aporte;
+    else
+      aportesAnualesFuente(contFuente,1) = anio;
+      aportesAnualesFuente(contFuente,2) = fuente;
+      aportesAnualesFuente(contFuente,3) = aporte;
+      contFuente += 1;
+    endif
+  endfor
+  disp(aportesAnualesFuente);
+  for i = 1:rows(energiasRenovables)
+    anio = energiasRenovables(i,1);
+    region = energiasRenovables(i,5);
+    aporte = energiasRenovables(i,6);
+    existePos = existeEnMatriz(aportesAnualesRegion, [ anio region ], [ 1 2 ]);
+    if existePos != -1
+      aportesAnualesRegion(existePos,3) += aporte;
+    else
+      aportesAnualesRegion(contRegion,1) = anio;
+      aportesAnualesRegion(contRegion,2) = region;
+      aportesAnualesRegion(contRegion,3) = aporte;
+      contRegion += 1;
+    endif
+  endfor
+  disp(aportesAnualesRegion);
 endfunction
 
 function ejercicio_F();
-  
+  energiasRenovables = load('EnergiasRenovablesSimple.dat');
+  totalAnual = [];
+  totalMensual = [];
+  contAnual = 1;
+  contMensual = 1;
+  for i = 1:rows(energiasRenovables)
+    anio = energiasRenovables(i,1);
+    aporte = energiasRenovables(i,6);
+    pos = existeEnMatriz(totalAnual, [ anio ], [ 1 ]);
+    if pos != -1
+      totalAnual(pos, 2) += aporte;
+    else
+      totalAnual(contAnual, 1) = anio;
+      totalAnual(contAnual, 2) = aporte;
+      contAnual += 1;
+    endif
+  endfor
+  for i = 1:rows(energiasRenovables)
+    anio = energiasRenovables(i,1);
+    mes = energiasRenovables(i,2);
+    aporte = energiasRenovables(i,6);
+    pos = existeEnMatriz(totalMensual, [ anio mes ], [ 1 2 ]);
+    if pos != -1
+      totalMensual(pos, 3) += aporte;
+    else
+      totalMensual(contMensual, 1) = anio;
+      totalMensual(contMensual, 2) = mes;
+      totalMensual(contMensual, 3) = aporte;
+      contMensual += 1;
+    endif
+  endfor
+  for i = 1:rows(totalAnual)
+    totalAnual(i,3) = totalAnual(i,2)*100/sum(totalAnual(:,2));
+  endfor
+  for i = 1:rows(totalMensual)
+    anio = totalMensual(i,1);
+    pos = existeEnMatriz(totalAnual, [ anio ], [ 1 ]);
+    totalMensual(i,4) = 100*totalMensual(i,3)/totalAnual(pos,2);
+  endfor
+  disp(totalAnual);
+  disp(totalMensual);
 endfunction
 
-function ejercicio_G();
-  
-  
+function ejercicio_G()
+  energiasRenovables = load('EnergiasRenovablesSimple.dat');
+  mayorCentralFuente = [];
+  for i = 1:6
+    mayorCentralFuente(i, 1) = i; #Fuente
+    mayorCentralFuente(i, 2) = -Inf; #Central
+    mayorCentralFuente(i, 3) = 0; #Mes
+    mayorCentralFuente(i, 4) = 0; #Region
+    mayorCentralFuente(i, 5) = 0; #Energia
+  endfor
+  for i = 1:rows(energiasRenovables)
+    fuente = energiasRenovables(i,4);
+    central = energiasRenovables(i,3);
+    mes = energiasRenovables(i,2);
+    region = energiasRenovables(i,5);
+    energia = energiasRenovables(i,6);
+    if central > mayorCentralFuente(fuente, 2)
+      mayorCentralFuente(fuente, 2) = central;
+      mayorCentralFuente(fuente, 3) = mes;
+      mayorCentralFuente(fuente, 4) = region;
+      mayorCentralFuente(fuente, 5) = energia;
+    endif
+  endfor
+  disp(mayorCentralFuente);
 endfunction
 
-function main()
-##  printf("----------Ejercicio A----------\n");
-##  ejercicio_A();
-##  printf("-------------------------------\n");
-##  printf("----------Ejercicio B----------\n");
-##  ejercicio_B();
-##  printf("-------------------------------\n");
+function f = main()
+  printf("----------Ejercicio A----------\n");
+  ejercicio_A();
+  printf("-------------------------------\n");
+  printf("----------Ejercicio B----------\n");
+  ejercicio_B();
+  printf("-------------------------------\n");
   printf("----------Ejercicio C----------\n");
   ejercicio_C();
   printf("-------------------------------\n");
-##  printf("----------Ejercicio D----------\n");
-##  ejercicio_D();
-##  printf("-------------------------------\n");
-##  printf("----------Ejercicio E----------\n");
-##  ejercicio_E();
-##  printf("-------------------------------\n");
-##  printf("----------Ejercicio F----------\n");
-##  ejercicio_F();
-##  printf("-------------------------------\n");
-##  printf("----------Ejercicio G----------\n");
-##  ejercicio_G();
-##  printf("-------------------------------\n");
+  printf("----------Ejercicio D----------\n");
+  ejercicio_D();
+  printf("-------------------------------\n");
+  printf("----------Ejercicio E----------\n");
+  ejercicio_E();
+  printf("-------------------------------\n");
+  printf("----------Ejercicio F----------\n");
+  ejercicio_F();
+  printf("-------------------------------\n");
+  printf("----------Ejercicio G----------\n");
+  ejercicio_G();
+  printf("-------------------------------\n");
 endfunction
 
-main();
+main()
