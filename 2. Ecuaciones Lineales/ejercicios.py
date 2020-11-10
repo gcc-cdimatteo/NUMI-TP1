9c71e3194177b0b8aba52d07b0ef7d636b53a033
Carolina <cdimatteo@fi.uba.ar>
2020-11-10 18:18:15 -0300

    Armo el esqueleto

---
+++ 2. Ecuaciones Lineales/ejercicios.py
@@ -0,0 +1,34 @@
+def ejercicioA():
+    print("Ejercicio A")
+
+def ejercicioB():
+    print("Ejercicio B")
+
+def ejercicioC():
+    print("Ejercicio C")
+
+def ejercicioD():
+    print("Ejercicio D")
+
+def ejercicioE():
+    print("Ejercicio E")
+
+def ejercicioF():
+    print("Ejercicio F")
+
+def main():
+    ej = [ "A", "B" ]
+    if "A" in ej:
+        ejercicioA()
+    if "B" in ej:
+        ejercicioB()
+    if "C" in ej:
+        ejercicioC()
+    if "D" in ej:
+        ejercicioD()
+    if "E" in ej:
+        ejercicioE()
+    if "F" in ej:
+        ejercicioF()
+
+main()
