25e722ea00a4ba6e86750e9c5f34e426eac4f289
Carolina <cdimatteo@fi.uba.ar>
2020-11-10 23:18:51 -0300

    trato de meter codigo de nico traducido a py

--- 2. Ecuaciones Lineales/ejercicios.py
+++ 2. Ecuaciones Lineales/ejercicios.py
@@ -1,5 +1,35 @@
-def ejercicioA():
-    print("Ejercicio A")
+RQ = int(0.2)
+MICROORGANISMO_CRECIMIENTO = [
+                                [RQ, 0, 1, 1, 0, 2] , 
+                                [1, 3, -1, -1, -1, -6] , 
+                                [-1, -1, 20, 0, 2, 1] , 
+                                [-1, -1, 0, 8, 4, 0] ,
+                                [0, 0, 2, 0, 7, 5]
+                            ]
+
+def elementos_diagonal(matriz):
+    elementos = []
+    dimension = len(matriz)
+    for i in range(dimension):
+        for j in range(dimension-1):
+            if i > j:
+                elementos.append(matriz[i][j])
+    return elementos
+            
+def matriz_triangulada(matriz):
+    diagonal = elementos_diagonal(matriz)
+    for elem in diagonal:
+        if elem != 0: 
+            return False
+    return True
+
+def ejercicioA(A,b):
+    dim = len(A[0])
+    for j in range(dim-2):
+        for i in range(j+1, dim):
+            m = A[i][j]/A[j][j]
+            A[i] = A[i] - m*A[j]
+    return
 
 def ejercicioB():
     print("Ejercicio B")
@@ -16,10 +46,24 @@ def ejercicioE():
 def ejercicioF():
     print("Ejercicio F")
 
+def armar_sistema(matriz):
+    A = []
+    b = []
+    for i in range(len(matriz)):
+        cont = 0
+        fila = []
+        while cont < len(matriz[i])-1:
+            fila.append(matriz[i][cont])
+            cont += 1
+        A.append(fila)
+        b.append(matriz[i][cont])
+    return A,b
+
 def main():
-    ej = [ "A", "B" ]
+    A,b = armar_sistema(MICROORGANISMO_CRECIMIENTO)
+    ej = [ "A" ]
     if "A" in ej:
-        ejercicioA()
+        ejercicioA(A,b)
     if "B" in ej:
         ejercicioB()
     if "C" in ej:
