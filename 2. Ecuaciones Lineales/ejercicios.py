d489bb43cb0bf8d56481463ef5f17addb9018581
Carolina <cdimatteo@fi.uba.ar>
2020-11-16 20:06:00 -0300

    upgrade: ej A

--- 2. Ecuaciones Lineales/ejercicios.py
+++ 2. Ecuaciones Lineales/ejercicios.py
@@ -1,10 +1,10 @@
 RQ = int(0.2)
 MICROORGANISMO_CRECIMIENTO = [
-                                [RQ, 0, 1, 1, 0, 2] , 
-                                [1, 3, -1, -1, -1, -6] , 
-                                [-1, -1, 20, 0, 2, 1] , 
-                                [-1, -1, 0, 8, 4, 0] ,
-                                [0, 0, 2, 0, 7, 5]
+                                [RQ, 0.0, 1.0, 1.0, 0.0, 2.0] , 
+                                [1.0, 3.0, -1.0, -1.0, -1.0, -6.0] , 
+                                [-1.0, -1.0, 20.0, 0.0, 2.0, 1.0] , 
+                                [-1.0, -1.0, 0.0, 8.0, 4.0, 0.0] ,
+                                [0.0, 0.0, 2.0, 0.0, 7.0, 5.0]
                             ]
 
 def abs_list(lista):
@@ -13,44 +13,49 @@ def abs_list(lista):
         absLista.append(abs(elem))
     return absLista
 
-def swap(matriz, pos1, pos2):
-    aux = matriz[pos1]
-    matriz[pos1] = matriz[pos2]
-    matriz[pos2] = aux
+def swap(A_sorted, pos1, pos2):
+    aux = A_sorted[pos1]
+    A_sorted[pos1] = A_sorted[pos2]
+    A_sorted[pos2] = aux
+
+def matriz_triangulada(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if i > j and matriz[i][j] != 0: return False
+    return True
+
+def fila_triangulada(matriz, pos_fila):
+    for i in range(len(matriz)):
+        if i != pos_fila: continue
+        for j in range(len(matriz[i])):
+            if i > j and matriz[i][j] != 0: return False
+    return True
 
 def ejercicioA(A,b):
+    # Ordeno
     dim = len(A)
     filA = {}
     for cont in range(dim):
         filA[cont] = abs_list(A[cont])
     filA_sorted = {k: v for k, v in sorted(filA.items(), key=lambda item: item[1], reverse=True)}
-    A_sorted = [[]*dim]*dim
-    cont = 0
+    A_sorted = []
     for fil in filA_sorted.keys():
-        A_sorted[cont] = A[fil]
-        cont += 1
-
-
-    h = 0
-    k = 0
-
-    while h < dim and k < dim:
-        i_max = 0
-        for i in range(h,dim):
-            if abs(A_sorted[i_max][k]) < abs(A_sorted[i][k]): i_max = i
-        
-        if A_sorted[i_max][k] == 0:
-            k = k+1
-        else:
-            swap(A_sorted, h, i_max)
-            for i in range(h + 1, dim):
-                f = A_sorted[i][k]/A_sorted[h][k]
-                A_sorted[i][k] = 0
-                for j in range(k + 1, dim):
-                    A_sorted[i][j] = A_sorted[i][j] - A_sorted[h][j] * f
-            h += 1
-            k += 1
-
+        A_sorted.append(A[fil])
+    #Triangulo
+    for i in range(len(A_sorted)):
+        if i == 0 or fila_triangulada(A_sorted, i): continue
+        indice = 0
+        for j in range(len(A_sorted[i])):
+            if A_sorted[i][j] != 0: 
+                indice = j
+                break
+        pivot = A_sorted[i-1][indice]
+        for k in range(i, len(A_sorted)):
+            if A_sorted[k][indice] == 0: continue
+            divisor = A_sorted[k][indice]
+            for j in range(len(A_sorted[k])):
+                A_sorted[k][j] = A_sorted[k][j]*pivot/divisor - A_sorted[i-1][j]
+    #Imprimo                    
     for elem in A_sorted:
         print(elem)
     return
@@ -70,17 +75,17 @@ def ejercicioE():
 def ejercicioF():
     print("Ejercicio F")
 
-def armar_sistema(matriz):
+def armar_sistema(A_sorted):
     A = []
     b = []
-    for i in range(len(matriz)):
+    for i in range(len(A_sorted)):
         cont = 0
         fila = []
-        while cont < len(matriz[i])-1:
-            fila.append(matriz[i][cont])
+        while cont < len(A_sorted[i])-1:
+            fila.append(A_sorted[i][cont])
             cont += 1
         A.append(fila)
-        b.append([matriz[i][cont]])
+        b.append([A_sorted[i][cont]])
     return A,b
 
 def main():
